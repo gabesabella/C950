edc8d542c378fdf5cacbea0844f103091b9b24f2
gabereal373 <gabereal373@gmail.com>
2022-10-17 19:09:23 -0400

    "Added truck class"

--- truck.py
+++ truck.py
@@ -1,26 +1,39 @@
-from hashtable import ChainingHashTable
 from package import packageHashTable
-
-from hashtable import ChainingHashTable
 from package import loadPackageData
-from package import packageHashTable
-
 
 loadPackageData('./csv/packages.csv')
 
+# If row is > col, swap
+
 
 class Truck:
-    def __init__(self, packages, departure, miles):
-        self.packageList = packages
-        self.departure = departure
-        self.miles = miles
+    def __init__(self):
+        # self.departure = departure
+        # self.time = date time object
+        self.miles = 0
+        self.currentLocation = '4001 South 700 East'
+        self.previousLocation = ''
+        self.newPackageList = [[]]
 
+    # setter method
 
-truck1 = ChainingHashTable()
+    def loadPackages(self, packageList):
+        newPackageList = [[]]
+        for i in range(len(packageList)):
+            newPackageList.insert(
+                packageList[i], packageHashTable.search(packageList[i]))
+        self.newPackageList = newPackageList
 
+    def getPackage(self, packageID):
+        newPackageList = self.newPackageList
+        for i in self.newPackageList:
+            if packageID == newPackageList[0]:
+                print("hmm maybe")
+            else:
+                print("Nope")
 
-# Truck 1 --> Departs at 8:00
-truckOnePackages = [1, 13, 14, 15, 16, 20, 29, 30, 31, 34, 40]
+            # Truck 1 --> Departs at 8:00
+truckOnePackages = [1, 13, 14, 15, 16, 20, 29, 30, 31, 34, 37, 40]
 # Package 15 due by 9:00, all others due by 10:30
 
 # Truck 2 --> Departs at 9:05
@@ -30,13 +43,35 @@ truckTwoPackages = [3, 6, 18, 25, 27, 28, 32, 33, 35,  36, 38, 39]
 # Truck 3 --> All EOD packages
 truckThreePackages = [2, 4, 5, 7, 8, 9, 10, 11, 12, 17, 19, 21, 22, 23, 24, 26]
 
-for i in range(len(truckOnePackages)):
-    truck1.insert(truckOnePackages[i],
-                  packageHashTable.search(truckOnePackages[i]))
+truck1 = Truck()
+truck1.loadPackages(truckOnePackages)
 
+truck2 = Truck()
+truck2.loadPackages(truckTwoPackages)
 
-print("Packages from package hash table:")
-for i in range(len(truck1.table)+1):
-    if truck1.search(i) == None:
-        continue
-    print("Package ID: {}".format(truck1.search(i)))
+truck3 = Truck()
+truck3.loadPackages(truckThreePackages)
+
+print(truck3.newPackageList[1])
+rows, cols = (5, 5)
+arr = [[0]*cols]*rows
+print(arr)
+
+# print(truck3.getPackage(4))
+
+# Load in distances and addresses as arrays
+# distanceData = [] --> 2d
+# addressData = [] --> 1d
+# for i in range(len(truckOnePackages)):
+#     truck1.insert(truckOnePackages[i],
+#                   packageHashTable.search(truckOnePackages[i]))
+
+# From call
+# for i in range len(truck1.packageList):
+#     print(truck1.packageList[i])
+
+# print("Packages from package hash table:")
+# for i in range(len(truck1)+1):
+#     # if truck1.search(i) == None:
+#     #     continue
+#     print(truck1[i])
